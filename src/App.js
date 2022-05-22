6899b02105e86448a12465f125fb85efc5538d27
Arameh Soltani <aramehsl@gmail.com>
2022-05-22 17:12:55 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a
-          href="https://www.linkedin.com/in/arameh-soltani/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Arameh soltani
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/AramehSL/weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://www.linkedin.com/in/arameh-soltani/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Arameh soltani
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/AramehSL/weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

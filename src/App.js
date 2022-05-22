474f5b60fa88789559b4f5663ecfa3060d286453
Arameh Soltani <aramehsl@gmail.com>
2022-05-22 16:51:10 -0400

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a
+          href="https://www.linkedin.com/in/arameh-soltani/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Arameh soltani
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/AramehSL/weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+      </footer>
+    </div>
+  );
 }

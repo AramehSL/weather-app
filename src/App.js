3461ba2afeaee2bbe2542d4ef1847f679031371d
Arameh Soltani <aramehsl@gmail.com>
2022-05-23 17:21:05 -0400

    Added API integration

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="Montreal" />
         <footer>
           This project is created by{" "}
           <a
--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              name="search"
-              placeholder="Enter a city ..."
-              className="form-control"
-              autoFocus="on"
-            />
-          </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
-      <h1>Montreal</h1>
-      <ul>
-        <li>Wednesday 17:17</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row">
-        <div className="col-6">
-          <div className="temp-info">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="Mostly cloudy"
-            />
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-            <span className="temperature">6</span>
-            <span className="unit">°C</span>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: Math.round(response.data.main.temp),
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+      date: "Wednesday 17:17",
+    });
+    console.log(response.data);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                name="search"
+                placeholder="Enter a city ..."
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation : 15%</li>
-            <li>Humidity : 80%</li>
-            <li>Wind : 11 km/h</li>
-          </ul>
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <div className="temp-info">
+              <img src={weatherData.iconUrl} alt={weatherData.description} />
+
+              <span className="temperature">{weatherData.temperature}</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity : {weatherData.humidity}%</li>
+              <li>Wind : {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "88ce603b3361b10f232b41d49c2e52e3";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading ... ";
+  }
 }
